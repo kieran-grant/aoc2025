078283301095be8e8b1dd18f5e0aed0001252803
Kieran Grant <>
2025-12-06 17:00:13 +0000

    solved day 6, part 2

--- bin/day6.ml
+++ bin/day6.ml
@@ -9,20 +9,15 @@ let () =
   let open Aoc2025.Day6 in
   (*Read inputs*)
   let input_txt = read_file "inputs/6.txt" in
-  let sample_input = parse_input sample in
-  let input = parse_input input_txt in
-
-  (* let sample_ranges, sample_ids = split_on_blank_line sample in *)
-  (* let ranges, ids = read_file "inputs/5.txt" |> split_on_blank_line in *)
-  (* List.iter (print_list print_string) input; *)
 
   (*Solve*)
-  let sample_sol_1 = part_1 sample_input in
-  let sol_1 = part_1 input in
-  (* let sample_sol_2 = part_2 sample_ranges in *)
-  (* let sol_2 = part_2 ranges in *)
+  let sample_sol_1 = part_1 sample in
+  let sol_1 = part_1 input_txt in
+  let sample_sol_2 = part_2 sample in
+  let sol_2 = part_2 input_txt in
+
   (*Print*)
   Printf.printf "Part 1 (sample): %d\n" sample_sol_1;
-  Printf.printf "Part 1: %d\n" sol_1
-(* Printf.printf "Part 2 (sample): %d\n" sample_sol_2; *)
-(* Printf.printf "Part 2: %d\n" sol_2 *)
+  Printf.printf "Part 1: %d\n" sol_1;
+  Printf.printf "Part 2 (sample): %d\n" sample_sol_2;
+  Printf.printf "Part 2: %d\n" sol_2
--- lib/day6.ml
+++ lib/day6.ml
@@ -1,11 +1,19 @@
 open Core
 
 let parse_input raw_txt =
-  let lines = String.split_on_char '\n' raw_txt in
-  let rows =
-    lines |> List.map split_on_whitespace |> List.filter (fun row -> row <> [])
+  let lines =
+    (*Split on lines*)
+    String.split_on_char '\n' raw_txt
+    (*Get rid of any empty lines*)
+    |> List.filter (fun x -> x <> "")
+    (*Then split on any whitespace in each line*)
+    |> List.map split_on_whitespace
   in
-  rows |> transpose |> List.map List.rev
+  lines
+  (*Turn columns to rows*)
+  |> transpose
+  (*Flip horizontally, makes pattern matching easier later*)
+  |> List.map List.rev
 
 let get_accumulator_fn x =
   match x with
@@ -18,4 +26,65 @@ let get_answer curr_list =
   | x :: xs -> (get_accumulator_fn x) (List.map int_of_string xs)
   | _ -> 0
 
-let part_1 lines = List.map get_answer lines |> sum
+let rec unsnoc = function
+  | [] -> failwith "Empty list!"
+  | [ x ] -> (x, [])
+  | x :: xs ->
+      let last, rest = unsnoc xs in
+      (last, x :: rest)
+
+let split_on_empty string_list =
+  let rec aux curr_list curr_acc acc =
+    match curr_list with
+    | [] ->
+        (* End of input: push the current group if non-empty *)
+        List.rev curr_acc :: acc
+    | x :: xs when x = "" ->
+        (* Empty string: finish current group and start a new one *)
+        aux xs [] (List.rev curr_acc :: acc)
+    | y :: ys ->
+        (* Normal element: accumulate it *)
+        aux ys (y :: curr_acc) acc
+  in
+  (* Compute and then reverse the final result so groups are in correct order *)
+  List.rev (aux string_list [] [])
+
+let prepend_each (ys : string list) (xs : string list list) =
+  let rec aux xs ys acc =
+    match (xs, ys) with
+    | x :: xs, y :: ys -> aux xs ys ((y :: x) :: acc)
+    | [], [] -> List.rev acc
+    | _ -> failwith "mismatched lists"
+  in
+  aux xs ys []
+
+let new_parse raw_txt =
+  let lines =
+    String.split_on_char '\n' raw_txt |> List.filter (fun x -> x <> "")
+  in
+  let last_row, rest = unsnoc lines in
+  rest
+  (*Turn string[] to char[][]*)
+  |> List.map explode
+  (*Transpose it*)
+  |> transpose
+  (*Turn it back into string[] (with some trailing whitespace)*)
+  |> List.map implode
+  (*Trim the whitespace*)
+  |> List.map String.trim
+  (*Turn string[] to string[][] by splitting on empty string*)
+  |> split_on_empty
+  (*Prepend each of the operators*)
+  |> prepend_each (split_on_whitespace last_row)
+
+let part_maker parser_fn raw_input =
+  raw_input
+  (*Parse input based on part*)
+  |> parser_fn
+  (*Do the sums*)
+  |> List.map get_answer
+  (*Use sum to accumulate the answer*)
+  |> sum
+
+let part_1 = part_maker parse_input
+let part_2 = part_maker new_parse
