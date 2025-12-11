d5d526e987195281c2329f26ce1f4ebe680dcfb7
Kieran Grant <>
2025-12-11 20:04:02 +0000

    Day 11, part 2 brute-forced (slow)

--- bin/day11.ml
+++ bin/day11.ml
@@ -1,4 +1,4 @@
-let sample =
+let sample_1 =
   {|aaa: you hhh
 you: bbb ccc
 bbb: ddd eee
@@ -10,19 +10,33 @@ ggg: out
 hhh: ccc fff iii
 iii: out|}
 
+let sample_2 =
+  {|svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out|}
+
 let _ =
   let open Aoc2025.Core in
   let open Aoc2025.Day11 in
   let input = read_file "inputs/11.txt" in
-  let sample_sol_1 = part_1 sample in
 
+  let sample_sol_1 = part_1 sample_1 in
   let sol_1 = part_1 input in
-
-  (* let sample_sol_2 = part_2 sample in *)
-  (* let sol_2 = part_2 input in *)
+  let sample_sol_2 = part_2 sample_2 in
+  let sol_2 = part_2 input in
 
   (*Print solutions*)
   Printf.printf "Part 1 (sample): %d\n" sample_sol_1;
-  Printf.printf "Part 1: %d\n" sol_1
-(* Printf.printf "Part 2 (sample): %d\n" sample_sol_2; *)
-(* Printf.printf "Part 2: %d\n" sol_2 *)
+  Printf.printf "Part 1: %d\n" sol_1;
+  Printf.printf "Part 2 (sample): %d\n" sample_sol_2;
+  Printf.printf "Part 2: %d\n" sol_2
--- lib/day11.ml
+++ lib/day11.ml
@@ -32,6 +32,44 @@ let find_num_paths (start : string) (target : string) adj_map =
   in
   dfs start
 
+let find_paths (start : string) (target : string) adj_map =
+  (* print_adj_map adj_map; *)
+  let memo : (string, string list list) Hashtbl.t = Hashtbl.create 32 in
+
+  let rec dfs node =
+    if node = target then [ [ target ] ]
+    else
+      match Hashtbl.find_opt memo node with
+      | Some lst -> lst
+      | None ->
+          let neighbors =
+            match StringMap.find_opt node adj_map with
+            | Some ls -> ls
+            | None -> failwith ("Node " ^ node ^ " could not be found!")
+          in
+          (* For each neighbor, get all paths and prepend current node *)
+          let paths =
+            List.concat_map
+              (fun neigh -> List.map (fun path -> node :: path) (dfs neigh))
+              neighbors
+          in
+          Hashtbl.add memo node paths;
+          paths
+  in
+  dfs start
+
+(* (* Print a single path as "A -> B -> C" *) *)
+(* let print_path path = *)
+(*   match path with *)
+(*   | [] -> () *)
+(*   | hd :: tl -> *)
+(*       print_string hd; *)
+(*       List.iter (fun node -> print_string (" -> " ^ node)) tl; *)
+(*       print_newline () *)
+
+(* Print a list of paths *)
+(* let print_paths paths = List.iter print_path paths *)
+
 let parse_line line =
   let parts = String.split_on_char ':' line in
   let key = List.hd parts in
@@ -45,4 +83,15 @@ let parse_input raw_txt =
   let lines = split_lines raw_txt |> List.map parse_line in
   List.fold_left (fun m (k, v) -> StringMap.add k v m) StringMap.empty lines
 
+let get_valid_paths (paths : string list list) =
+  List.filter
+    (fun path ->
+      List.exists (fun x -> x = "fft") path
+      && List.exists (fun x -> x = "dac") path)
+    paths
+
 let part_1 raw_text = raw_text |> parse_input |> find_num_paths "you" "out"
+
+let part_2 raw_text =
+  raw_text |> parse_input |> find_paths "svr" "out" |> get_valid_paths
+  |> List.length
